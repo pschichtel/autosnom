535ba658d4a72860d184a8bbdce062cd93f1d424
Phillip Schichtel <phillip@schich.tel>
2019-11-19 01:55:53 +0100

    handle missing XML files

--- src/main/scala/controllers/MainController.scala
+++ src/main/scala/controllers/MainController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.io.FileNotFoundException
 import java.nio.file.{Path, Paths}
 
 import javax.inject.Inject
@@ -22,10 +23,19 @@ class MainController @Inject() (cc: ControllerComponents, conf: Configuration) e
         val typeConf = loadXML(typeConfPath)
         val deviceConf = loadXML(deviceConfPath)
 
-        Ok(mergeTrees(typeConf, deviceConf))
+        (typeConf, deviceConf) match {
+            case (Some(t), None) => Ok(t)
+            case (None, Some(d)) => Ok(d)
+            case (Some(t), Some(d)) => Ok(mergeTrees(t, d))
+            case (None, None) => NotFound
+        }
     }
 
-    private def loadXML(path: Path): Elem = scala.xml.XML.loadFile(path.toFile)
+    private def loadXML(path: Path): Option[Elem] = try {
+        Some(scala.xml.XML.loadFile(path.toFile))
+    } catch {
+        case _: FileNotFoundException => None
+    }
 
     private def mergeTrees(left: Elem, right: Elem): Elem = {
         left.copy(child = left.child ++ right.child)
